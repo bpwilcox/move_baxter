dee3b07f1a2d23827a19ba90fba77d1fae2960c5
jacobjj <jacobjohnjohnson94@gmail.com>
2018-07-10 10:12:02 -0700

    Added joint limit test function

--- src/baxter_hapticV2.py
+++ src/baxter_hapticV2.py
@@ -3,6 +3,7 @@ import rospy
 import baxter_interface
 from math import sin
 import numpy as np
+
 from transforms3d.quaternions import (
     mat2quat,
     quat2mat
@@ -49,12 +50,8 @@ class haptic_pos:
         rospy.Subscriber('pose_msg',Float32MultiArray,self.callback)
     def callback(self,data_stream):
         self.hd_transform = np.reshape(data_stream.data[0:16],(4,4),order='F')
-        #self.hd_transform = np.matmul(self.baxter_transform,self.hd_transform)
         self.hd_vel = np.asarray(data_stream.data[16:19])
-        #self.hd_vel = np.matmul(self.baxter_transform[0:3,0:3],self.hd_vel)
         self.hd_ang_vel = np.asarray(data_stream.data[19:22])
-        #self.hd_ang_vel = np.matmul(self.baxter_transform[0:3,0:3],self.hd_ang_vel)
-        #self.hd_position = np.asarray(data_stream.data[22:25])
         if data_stream.data[22]==1:
             self.hd_button1 = True
         else:
@@ -66,6 +63,11 @@ class haptic_pos:
 
 def R_offset(hd_transform_0,b_transform_0):
     return np.matmul(hd_transform_0.T,b_transform_0)
+
+def scale_x(x):
+    scale_mat = np.diag([1.0/160,1.0/70,1.0/200])
+    #scale_mat = np.eye(3)
+    return np.matmul(scale_mat,x)
 
 def main():
     log_joint_angles = []
@@ -99,7 +101,12 @@ def main():
         #np.save('log_haptic.npy',np.asarray(log_haptic))
         limb.move_to_neutral()
         rs.disable()
-    x = 0
+
+    alpha = 1
+    b_x = np.asarray([x_i for x_i in limb.endpoint_pose()['position']])
+    hd_x = scale_x(np.matmul(baxter_transform,phantom.hd_transform[0:3,3]))
+    x_off = b_x - hd_x*alpha
+
     hd_transform_0 = np.matmul(baxter_transform,phantom.hd_transform[0:3,0:3])
     b_ori_q = limb.endpoint_pose()['orientation']
     b_transform_0 = quat2mat([b_ori_q.w,b_ori_q.x,b_ori_q.y,b_ori_q.z])
@@ -112,25 +119,17 @@ def main():
         old_joint_angles = limb.joint_angles()
         cur_pos = limb.endpoint_pose()['position']
         des_ori = np.matmul(np.matmul(baxter_transform,phantom.hd_transform[0:3,0:3]),R_off)
-
         ori = mat2quat(des_ori)
+
         cur_ori = limb.endpoint_pose()['orientation']
-        des_ori  = baxter_interface.Limb.Quaternion(w=ori[0],x=ori[1],y=ori[2],z=ori[3])
-        #Get increment from haptic device
-        alpha = 0.001
-        delta_pos = alpha*np.matmul(baxter_transform,phantom.hd_vel)
-        #print(delta_pos)
-        #log_haptic.append([delta_pos[0],delta_pos[1],delta_pos[2]])
+        des_ori_command  = baxter_interface.Limb.Quaternion(w=ori[0],x=ori[1],y=ori[2],z=ori[3])
+        hd_x = scale_x(np.matmul(baxter_transform,phantom.hd_transform[0:3,3]))
+        des_x = alpha*hd_x + x_off
+
         if not phantom.hd_button1:
-            #Baxter's new position
-            new_pos = Point(cur_pos.x+delta_pos[0],cur_pos.y+delta_pos[1],cur_pos.z+delta_pos[2])
-            des_pos = new_pos
-            #IK pose
-            #pose= PoseStamped(header=hdr,pose=Pose(position= new_pos,orientation=cur_ori))
-            pose= PoseStamped(header=hdr,pose=Pose(position= new_pos,orientation=des_ori))
-            #seed_angle = JointState(header=hdr,name=limb.joint_names(),position=np.asarray([limb.joint_angle(joint) for joint in limb.joint_names()]))
+            des_pos = Point(des_x[0],des_x[1],des_x[2])
+            pose= PoseStamped(header=hdr,pose=Pose(position= des_pos,orientation=des_ori_command))
             ikreq.pose_stamp.append(pose)
-            #ikreq.seed_angles.append(seed_angle)
             resp = iksvc(ikreq)
 
             if (resp.isValid[0]):
@@ -140,32 +139,22 @@ def main():
                 #print limb_joints
             else:
                 print("INVALID POSE - No Valid Joint Solution Found.")
-                new_pos = cur_pos
+                des_pos = cur_pos
                 limb_joints = old_joint_angles
 
-            # Baxter joint control
-            #for j in limb.joint_names():
-            #    limb_joints[j] = limb_joints[j]*0.5 + old_joint_angles[j]*0.5
-
-            #Log data
-            #log_joint_angles.append([limb_joints[j] for j in limb.joint_names()])
-            #log_pos.append([new_pos.x,new_pos.y,new_pos.z])
-
-            #Move Baxter Joint
-            #des_joint_angles = np.asarray([old_joint_angles[j] for j in limb.joint_names()])+ [np.sin(x)*.01,0.01*np.sin(x),0.01*np.sin(x),0.01*np.sin(x),0.01*np.sin(x),0.01*np.sin(x),0.01*np.sin(x)]
-
-            x+=0.01
-            #des_joint_angles = old_joint_angles+np.asarray([0,4*sin(2*3.14*x),0,0,0,0,0])
-            #limb_joints = dict(zip(limb.joint_names(),des_joint_angles))
             limb.set_joint_positions(limb_joints,raw=True)
             ikreq.pose_stamp.pop()
-            #ikreq.seed_angles.pop()
         else:
-            #Reset R_offset
+            while phantom.hd_button1:
+                pass
             hd_transform_0 = np.matmul(baxter_transform,phantom.hd_transform[0:3,0:3])
             b_ori_q = limb.endpoint_pose()['orientation']
             b_transform_0 = quat2mat([b_ori_q.w,b_ori_q.x,b_ori_q.y,b_ori_q.z])
             R_off = R_offset(hd_transform_0,b_transform_0)
+            b_x = np.asarray([x_i for x_i in limb.endpoint_pose()['position']])
+            hd_x = scale_x(np.matmul(baxter_transform,phantom.hd_transform[0:3,3]))
+            x_off = b_x - hd_x*alpha
+
         rate.sleep()
 
     limb.move_to_neutral()
--- src/visualize_haptic_pose.py
+++ src/visualize_haptic_pose.py
@@ -25,9 +25,9 @@ from std_msgs.msg import (
     Float32MultiArray
 )
 
+from visualizations_msgs.msg import Marker
 
 from baxter_core_msgs.msg import SEAJointState
-from visualization_msgs.msg import Marker
 
 from baxter_interface import CHECK_VERSION
 
@@ -64,40 +64,29 @@ class haptic_pos:
         else:
             self.hd_button2 = False
 
-
-class gravity_compensation:
-    def __init__(self):
-        self.gravity_torque = np.zeros((7))
-        node_right = 'robot/limb/right/gravity_compensation_torques'
-        rospy.Subscriber(node_right,SEAJointState,self.callback)
-    def callback(self,data_stream):
-        self.gravity_torque = np.asarray(data_stream.gravity_model_effort[0:7])
-
 def scale_x(x):
     scale_mat = np.diag([1.0/160,1.0/70,1.0/200])
     #scale_mat = np.eye(3)
     return np.matmul(scale_mat,x)
 
-
-def joint_limit_test(b_joint_angles):
-    #A function to test the joint limit of the system
-    eps = 0.01
-    joint_max = np.asarray([1.7016,1.047,3.0541,2.618,3.059,2.094,3.059]) - eps
-    joint_min = np.asarray([-1.7016,-2.147,-3.0541,-0.05,-3.059,-1.5707,-3.059]) +eps
-    if(any(b_joint_angles<joint_min) or any(b_joint_angles>joint_max)):
-        print('Joint limit reached')
-
 def main():
-    log_x_des = []
-    log_x_b = []
-    log_hd = []
-    log_joint_torque = []
-    log_delta_theta = []
-    rospy.init_node('Control_baxter')
+    rospy.init_node('HapticPoint')
 
     #Creating the Publisher for rviz visualization
-    rviz_pub = rospy.Publisher('hd_point',PoseStamped,queue_size=10)
-    hdr = Header(stamp=rospy.Time.now(), frame_id='/world')
+    rviz_pub = rospy.Publisher('hd_point',Marker,queue_size=10)
+
+
+    #Creating the Point
+    hd_point = Marker()
+    hd_point.header.frame_id = '/world'
+    hd_point.type = Marker.POINTS
+    hd_point.scale.x = 0.45
+    hd_point.scale.y = 0.45
+    hd_point.color.r = 0.0
+    hd_point.color.g = 0.5
+    hd_point.color.b = 0.5
+    hd_point.color.a = 1.0
+
 
     #Initalize Subscriber for the Haptic device
     phantom = haptic_pos()
@@ -122,19 +111,11 @@ def main():
     rate = rospy.Rate(1000)
 
     def reset_baxter():
-        np.save('log_joint_torque.npy',np.asarray(log_joint_torque))
-        np.save('log_x_des.npy',np.asarray(log_x_des))
-        np.save('log_x_b.npy',np.asarray(log_x_b))
-        np.save('log_hd.npy',np.asarray(log_hd))
-        np.save('log_delta_theta.npy',np.asarray(log_delta_theta))
         limb.move_to_neutral()
         rs.disable()
-    x = 0
-    #K = 100
+
     K = np.diag([30,30,30,1,1,1])*0.5
-    #K_v = np.diag([1,1,1,1,1,1,1])*10
     K_v = np.diag([1,1,1,1,1,1])
-    #K_v = 2*np.sqrt(K)
 
     alpha = 1
     rospy.on_shutdown(reset_baxter)
@@ -142,20 +123,20 @@ def main():
     hd_transform_0 = np.matmul(baxter_transform,phantom.hd_transform[0:3,0:3])
     b_ori_q = limb.endpoint_pose()['orientation']
     b_transform_0 = quat2mat([b_ori_q.w,b_ori_q.x,b_ori_q.y,b_ori_q.z])
-
     R_off = R_offset(hd_transform_0,b_transform_0)
+    R_off = mat2quat(R_off)
+
     b_x = np.asarray([x_i for x_i in limb.endpoint_pose()['position']])
     hd_x = scale_x(np.matmul(baxter_transform,phantom.hd_transform[0:3,3]))
     x_off = b_x - hd_x*alpha
     while not rospy.is_shutdown():
-        M_ee = kin.cart_inertia()
-        #print(J_T)
         joint_angles = limb.joint_angles()
 
         #Get posd_t from haptic device
         hd_ori = np.matmul(baxter_transform,phantom.hd_transform[0:3,0:3])
+        des_ori = np.matmul(hd_ori,R_off)
         hd_x = scale_x(np.matmul(baxter_transform,phantom.hd_transform[0:3,3]))
-        #print(hd_x)
+
         #Baxter's orientation
         b_q = limb.endpoint_pose()['orientation']
         b_x = np.asarray([x_i for x_i in limb.endpoint_pose()['position']])
@@ -166,11 +147,6 @@ def main():
         if not phantom.hd_button1:
             des_R = np.matmul(hd_ori,R_off)
             des_x = alpha*hd_x + x_off
-            #print(hd_x,des_x)
-            log_x_des.append(des_x)
-            log_hd.append(hd_x)
-            log_x_b.append(b_x)
-
         else:
             print("Button 1 Pressed")
             while phantom.hd_button1:
@@ -205,72 +181,24 @@ def main():
             R_off = R_offset(hd_transform_0,b_transform_0)
 
         des_ori = mat2quat(des_R)
-        pos = Point(des_x[0],des_x[1],des_x[2])
-        ori = Quaternion(w = des_ori[0], x = des_ori[1], y = des_ori[2], z = des_ori[3])
-        pose= PoseStamped(header=hdr,pose=Pose(position= pos,orientation= ori))
-        rviz_pub.publish(pose)
+        hd_point.pose.orientation.w = des_ori[0]
+        hd_point.pose.orientation.x = des_ori[1]
+        hd_point.pose.orientation.y = des_ori[2]
+        hd_point.pose.orientation.z = des_ori[3]
+
+        hd_point.pose.position.x = des_x[0]
+        hd_point.pose.position.y = des_x[1]
+        hd_point.pose.position.z = des_x[2]
 
         J_T = kin.jacobian(pos=[0,0,0]).T
-        delta_R = np.matmul(b_tranform.T,des_R)
+        delta_R = np.matmul(b_tranform.T,des_ori)
         delta_theta = np.asarray(transfE.mat2euler(delta_R)).reshape((3,1))
-        log_delta_theta.append(delta_theta)
         delta_x = np.asarray(des_x-b_x).reshape((3,1))
         delta_pos = np.concatenate((delta_x,delta_theta))
         #delta_pos = np.concatenate((alpha*hd_vel,np.zeros((3,1))))
+        rviz_pub.publish(hd_point)
 
-        #Finding the velocity error
-        hd_l_vel = np.matmul(baxter_transform,phantom.hd_vel)*0.001
-        hd_a_vel = np.matmul(baxter_transform,phantom.hd_ang_vel)
-
-        des_vel = np.concatenate((hd_l_vel,hd_a_vel))
-        b_joint_vel = np.asarray([limb.joint_velocity(name) for name in limb.joint_names()])
-        b_end_vel = np.matmul(J_T.T,b_joint_vel)
-        delta_vel = des_vel.reshape((6,1)) - b_end_vel.reshape((6,1))
-
-        des_force = np.matmul(K,delta_pos)+np.matmul(K_v,delta_vel)
-
-        #des_joint_torques = np.matmul(J_T,np.matmul(M_ee,des_acc))
-        des_joint_torques = np.matmul(J_T,des_force)
-        #des_joint_torques = np.matmul(kin.inertia(),des_joint_torques)
-        #print("Delta x: {}\n Delta vel: {}\n".format(delta_pos,delta_vel))
-
-        #Finding the torque for the secondary controller
-        b_q = np.asarray([limb.joint_angle(joint) for joint in limb.joint_names()])
-        K_null = np.diag([0,0,1,0,0,0,0])*10
-        K_V_null = np.diag([0,0,1,0,0,0,0])
-        des_joint_angles = np.asarray([0,0,0,0,0,0,0])
-        #This is to keep the E_0 joint at 0deg
-        des_joint_torque_null  = np.matmul(K_null,des_joint_angles-b_q) + np.matmul(K_V_null,-b_joint_vel)
-
-        #Finding the projection operator for the null torque opreration
-        #J_T_pinv = np.matmul(kin.cart_inertia(),np.matmul(kin.jacobian(),np.linalg.inv(kin.inertia())))
-        J_T_pinv = np.matmul(np.linalg.inv(np.matmul(J_T.T,J_T)),J_T.T)
-        P_null = np.eye(7) - np.matmul(J_T,J_T_pinv)
-        T_null = np.asarray(np.matmul(P_null,des_joint_torque_null)).reshape((7,))
-
-        #clip the toruqes:
-        #tor_lim = 1000
-        tor_lim = np.asarray([50,50,0,50,15,15,15])*2
-        des_joint_torques = np.diag(np.clip(des_joint_torques,-tor_lim,tor_lim)).reshape((7,))
-        #des_joint_torques = des_joint_torques-np.matmul(K_v,b_joint_vel)+grv_comp.gravity_torque/100
-        #print(des_joint_torques)
-        des_joint_torques = des_joint_torques+grv_comp.gravity_torque/100 + T_null
-        #print("Total Torque: {}".format(des_joint_torques))
-
-        #des_joint_torques = np.zeros(7)#+grv_comp.gravity_torque/100
-        #print("Rotation Matrix {} \n Orientation {} joint torques {}".format(delta_R,delta_theta.T,des_joint_torques.T))
-        #print(grv_comp.gravity_torque)
-        #print(des_joint_torques.T)
-        log_joint_torque.append(des_joint_torques)
-        limb_torques = dict(zip(limb.joint_names(),des_joint_torques))
-        limb.set_joint_torques(limb_torques)
-        b_joint_angles = np.asarray([limb.joint_angle(joint) for joint in limb.joint_names()])
-        print("Joint angles {}".format(b_joint_angles))
-        joint_limit_test(b_joint_angles)
-        #print(limb_torques)
-        #rospy.spin()
         rate.sleep()
-    print()
     limb.move_to_neutral()
     rs.disable()
 
